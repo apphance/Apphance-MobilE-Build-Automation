c0f224e84ad13a0732b953152329ecff074a2ba7
Marcin Marek Gocal <marcin.gocal@gmail.com>
2012-03-07 09:17:09 +0100

    renamed "show wp7 properties" task

--- src/main/groovy/com/apphance/ameba/wp7/plugins/buildplugin/ShowWp7PropertiesOperation.groovy
+++ src/main/groovy/com/apphance/ameba/wp7/plugins/buildplugin/ShowWp7PropertiesOperation.groovy
@@ -2,11 +2,9 @@ package com.apphance.ameba.wp7.plugins.buildplugin
 
 import com.apphance.ameba.AbstractShowSetupOperation
 
-class ShowWp7SetupOperation extends AbstractShowSetupOperation {
+class ShowWp7PropertiesOperation extends AbstractShowSetupOperation {
 
-	ShowWp7SetupOperation() {
+	ShowWp7PropertiesOperation() {
 		super(Wp7ProjectProperty.class)
 	}
-
-
 }
