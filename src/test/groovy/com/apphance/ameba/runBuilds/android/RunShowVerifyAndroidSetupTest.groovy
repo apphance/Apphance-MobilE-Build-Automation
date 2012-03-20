f4bb9c8319f175dd1718a7c2eb2ce84d4b3017d9
Jarek Potiuk <jarek@potiuk.com>
2012-03-20 23:01:49 +0100

    More test run issues fixed

--- src/test/groovy/com/apphance/ameba/runBuilds/android/RunShowVerifyAndroidSetupTest.groovy
+++ src/test/groovy/com/apphance/ameba/runBuilds/android/RunShowVerifyAndroidSetupTest.groovy
@@ -1,28 +1,28 @@
-package com.apphance.ameba.runBuilds.ios;
+package com.apphance.ameba.runBuilds.android;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*
 
-import java.io.File;
+import java.io.File
 
 import org.gradle.tooling.BuildLauncher
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.ProjectConnection
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
 
 
-class RunShowVerifySetupTest {
-    static File testIosProject = new File("testProjects/ios/GradleXCode")
-    static ProjectConnection connection
+class RunShowVerifyAndroidSetupTest {
+    static File testIosProject = new File("testProjects/android")
+    static ProjectConnection connection = GradleConnector.newConnector().forProjectDirectory(testIosProject).connect();
 
     @BeforeClass
-    static beforeClass() {
-        connection = GradleConnector.newConnector().forProjectDirectory(testIosProject).connect()
+    static void beforeClass() {
+        connection = GradleConnector.newConnector().forProjectDirectory(testIosProject).connect();
     }
 
     @AfterClass
-    static afterClass() {
+    static void afterClass() {
         connection.close()
     }
 
@@ -40,9 +40,9 @@ class RunShowVerifySetupTest {
     @Test
     public void testShowSetup() {
         String res = runTests('showSetup')
-        assertTrue(res.contains('# iOS properties'))
-        assertTrue(res.contains('# iOS FoneMonkey properties'))
-        assertTrue(res.contains('# iOS Framework properties'))
+        assertTrue(res.contains('# Android properties'))
+        assertTrue(res.contains('# Android jar library properties'))
+        assertTrue(res.contains('# Android test properties'))
         assertTrue(res.contains('# Mercurial properties'))
         assertTrue(res.contains('# Release properties'))
     }
@@ -50,9 +50,9 @@ class RunShowVerifySetupTest {
     @Test
     public void testVerifySetup() {
         String res = runTests('verifySetup')
-        assertTrue(res.contains('GOOD!!! iOS properties'))
-        assertTrue(res.contains('GOOD!!! iOS FoneMonkey properties'))
-        assertTrue(res.contains('GOOD!!! iOS Framework properties'))
+        assertTrue(res.contains('GOOD!!! Android properties'))
+        assertTrue(res.contains('GOOD!!! Android jar library properties'))
+        assertTrue(res.contains('GOOD!!! Android test properties'))
         assertTrue(res.contains('GOOD!!! Mercurial properties'))
         assertTrue(res.contains('GOOD!!! Release properties'))
     }
