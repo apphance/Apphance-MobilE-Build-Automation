9f219f23c6e0f60256bc44c09c5d9d14129b7146
Przemysław Jakubczyk <przemyslaw.jakubczyk@polidea.pl>
2012-04-19 14:29:45 +0200

    Convention for Robotium and Robolectic path https://dev.polidea.pl/redmine/issues/6457

--- src/test/groovy/com/apphance/ameba/conventions/TestConventions.groovy
+++ src/test/groovy/com/apphance/ameba/conventions/TestConventions.groovy
@@ -14,32 +14,57 @@ import com.apphance.ameba.ProjectHelper
 import com.apphance.ameba.unit.EmmaDumper
 
 class TestConventions {
-    private static File conventionsBase = new File("testProjects/conventions")
+	private static File conventionsBase = new File("testProjects/conventions")
 
-    private ProjectConnection getProjectConnection(File baseFolder, String dirName) {
-        def projectDir = new File(baseFolder, dirName)
-        return  GradleConnector.newConnector().forProjectDirectory(projectDir).connect()
-    }
+	private ProjectConnection getProjectConnection(File baseFolder, String dirName) {
+		def projectDir = new File(baseFolder, dirName)
+		return  GradleConnector.newConnector().forProjectDirectory(projectDir).connect()
+	}
 
-    @Test
-    public void testAndroidAnalysisConvention() throws Exception {
-         ProjectConnection connection = getProjectConnection(conventionsBase,"ameba-android-analysis")
-         try {
-             BuildLauncher bl = connection.newBuild().forTasks('showConventionAndroidAnalysis');
-             ByteArrayOutputStream baos = new ByteArrayOutputStream()
-             bl.setStandardOutput(baos)
-             bl.setJvmArguments(ProjectHelper.GRADLE_DAEMON_ARGS)
-             bl.run()
-             String output = baos.toString('utf-8')
-             println output
-             assertTrue(output.contains('androidAnalysis {'))
-         } finally {
-             connection.close()
-         }
-    }
+	@Test
+	public void testAndroidAnalysisConvention() throws Exception {
+		ProjectConnection connection = getProjectConnection(conventionsBase,"ameba-android-analysis")
+		try {
+			BuildLauncher bl = connection.newBuild().forTasks('showConventionAndroidAnalysis');
+			ByteArrayOutputStream baos = new ByteArrayOutputStream()
+			bl.setStandardOutput(baos)
+			bl.setJvmArguments(ProjectHelper.GRADLE_DAEMON_ARGS)
+			bl.run()
+			String output = baos.toString('utf-8')
+			println output
+			assertTrue(output.contains('androidAnalysis {'))
+		} finally {
+			connection.close()
+		}
+	}
+	@Test
+	public void testAndroidTestConvention() {
+		ProjectConnection connection = getProjectConnection(conventionsBase,"ameba-android-test")
+		try {
+			BuildLauncher bl = connection.newBuild().forTasks('showConventionAndroidTest');
+			ByteArrayOutputStream baos = new ByteArrayOutputStream()
+			bl.setStandardOutput(baos)
+			bl.setJvmArguments(ProjectHelper.GRADLE_DAEMON_ARGS)
+			bl.run()
+			String output = baos.toString('utf-8')
+			println output
+			assertTrue(output.contains('androidTest {'))
+			assertTrue(output.contains('startPort'))
+			assertTrue(output.contains('endPort'))
+			assertTrue(output.contains('maxEmulatorStartupTime'))
+			assertTrue(output.contains('retryTime'))
+			assertTrue(output.contains('robotiumPath'))
+			assertTrue(output.contains('robolectricPath'))
+		} finally {
+			connection.close()
+		}
+	}
 
-    @AfterClass
-    static public void afterClass() {
-        EmmaDumper.dumpEmmaCoverage()
-    }
+
+
+
+	@AfterClass
+	static public void afterClass() {
+		EmmaDumper.dumpEmmaCoverage()
+	}
 }
