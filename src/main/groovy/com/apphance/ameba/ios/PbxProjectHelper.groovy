c6ceb260c4f6c811bb9c3336930855855b8aa85e
Maciej Sawicki <maciej.sawicki@polidea.pl>
2012-05-18 00:59:16 +0200

    -fixed var definition

--- src/main/groovy/com/apphance/ameba/ios/PbxProjectHelper.groovy
+++ src/main/groovy/com/apphance/ameba/ios/PbxProjectHelper.groovy
@@ -342,7 +342,7 @@ class PbxProjectHelper {
                 value: 'APHLog', summary: true) {
                     fileset(dir: projectRootDir) {
                         files.each {
-                            property = getProperty(getObject(it.text()), "fileRef")
+                            def property = getProperty(getObject(it.text()), "fileRef")
                             if (property != null){
                             	include (name : objects[property.text()])
 			    }
