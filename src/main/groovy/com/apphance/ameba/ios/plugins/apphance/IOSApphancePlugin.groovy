f9ad6fcc25127917e79c893ed362058375df494a
maciej-sawicki <maciej.sawicki@polidea.pl>
2012-06-01 16:24:21 +0300

    -fixed "No such property: libsApphance" bug

--- src/main/groovy/com/apphance/ameba/ios/plugins/apphance/IOSApphancePlugin.groovy
+++ src/main/groovy/com/apphance/ameba/ios/plugins/apphance/IOSApphancePlugin.groovy
@@ -116,7 +116,7 @@ class IOSApphancePlugin implements Plugin<Project> {
 
 
         def projectApphanceZip =new File(libsDir, "apphance.zip")
-        libsApphance.delete()
+        projectApphanceZip.delete()
         def apphanceUrl = new URL("http://dev.polidea.pl/ext/32092342903/latest_ios_apphance.zip")
         projectApphanceZip << apphanceUrl.getContent()
 	projectApphanceZip.close()
