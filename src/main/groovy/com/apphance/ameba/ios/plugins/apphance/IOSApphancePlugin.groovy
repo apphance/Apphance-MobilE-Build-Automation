20c049dd23fe9a30d1fa5c684a2f692168dae6b3
Maciej Opała <maciek.opala@gmail.com>
2013-01-09 12:19:47 +0100

    formatting

--- src/main/groovy/com/apphance/ameba/ios/plugins/apphance/IOSApphancePlugin.groovy
+++ src/main/groovy/com/apphance/ameba/ios/plugins/apphance/IOSApphancePlugin.groovy
@@ -1,15 +1,5 @@
 package com.apphance.ameba.ios.plugins.apphance
 
-import groovy.io.FileType
-
-import java.io.File
-
-import org.gradle.api.GradleException
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
-
 import com.apphance.ameba.ProjectConfiguration
 import com.apphance.ameba.ProjectHelper
 import com.apphance.ameba.PropertyCategory
@@ -22,6 +12,11 @@ import com.apphance.ameba.ios.IOSXCodeOutputParser
 import com.apphance.ameba.ios.PbxProjectHelper
 import com.apphance.ameba.ios.plugins.buildplugin.IOSPlugin
 import com.apphance.ameba.ios.plugins.buildplugin.IOSSingleVariantBuilder
+import groovy.io.FileType
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 
 /**
  * Plugin for all apphance-relate IOS tasks.
@@ -40,7 +35,7 @@ class IOSApphancePlugin implements Plugin<Project> {
 
     public void apply(Project project) {
         ProjectHelper.checkAllPluginsAreLoaded(project, this.class, IOSPlugin.class)
-        use (PropertyCategory) {
+        use(PropertyCategory) {
             this.projectHelper = new ProjectHelper()
             this.conf = project.getProjectConfiguration()
             this.iosXcodeOutputParser = new IOSXCodeOutputParser()
@@ -48,7 +43,7 @@ class IOSApphancePlugin implements Plugin<Project> {
             this.iosSingleVariantBuilder = new IOSSingleVariantBuilder(project, project.ant)
             this.pbxProjectHelper = new PbxProjectHelper()
 
-            def trimmedListOutput = projectHelper.executeCommand(project, ["xcodebuild", "-list"]as String[],false, null, null, 1, true)*.trim()
+            def trimmedListOutput = projectHelper.executeCommand(project, ["xcodebuild", "-list"] as String[], false, null, null, 1, true)*.trim()
             iosConf.configurations = iosXcodeOutputParser.readBuildableConfigurations(trimmedListOutput)
             iosConf.targets = iosXcodeOutputParser.readBuildableTargets(trimmedListOutput)
             preprocessBuildsWithApphance(project)
@@ -64,7 +59,7 @@ class IOSApphancePlugin implements Plugin<Project> {
             iosConf.targets.each { target ->
                 def variant = "${target}-${configuration}".toString()
                 if (!iosConf.isBuildExcluded(variant)) {
-                    def noSpaceId = variant.replaceAll(' ','_')
+                    def noSpaceId = variant.replaceAll(' ', '_')
                     def singleTask = project."build-${noSpaceId}"
                     addApphanceToTask(project, singleTask, variant, target, configuration, iosConf)
                 }
@@ -80,7 +75,7 @@ class IOSApphancePlugin implements Plugin<Project> {
             def builder = new IOSSingleVariantBuilder(project, new AntBuilder())
             if (!isApphancePresent(builder.tmpDir(target, configuration))) {
                 logger.info("Adding Apphance to ${variant} (${target}, ${configuration}): " +
-                    "${builder.tmpDir(target, configuration)}. Project file = ${projConf.xCodeProjectDirectories[variant]}")
+                        "${builder.tmpDir(target, configuration)}. Project file = ${projConf.xCodeProjectDirectories[variant]}")
                 pbxProjectHelper.addApphanceToProject(builder.tmpDir(target, configuration),
                         projConf.xCodeProjectDirectories[variant], target, configuration, project[ApphanceProperty.APPLICATION_KEY.propertyName])
                 copyApphanceFramework(project, builder.tmpDir(target, configuration))
@@ -90,32 +85,32 @@ class IOSApphancePlugin implements Plugin<Project> {
 
     void replaceLogsWithApphance(Project project, File tmpDir) {
         logger.lifecycle("Replacing NSLog logs with Apphance in ${tmpDir}")
-        project.ant.replace(casesensitive: 'true', token : 'NSLog',
+        project.ant.replace(casesensitive: 'true', token: 'NSLog',
                 value: 'APHLog', summary: true) {
-                    fileset(dir: tmpDir) { include (name : '**/*.m') }
-                }
+            fileset(dir: tmpDir) { include(name: '**/*.m') }
+        }
     }
 
     private copyApphanceFramework(Project project, File libsDir) {
         logger.lifecycle("Copying apphance into directory " + libsDir)
         libsDir.mkdirs()
-        libsDir.traverse([type: FileType.FILES, maxDepth : ProjectHelper.MAX_RECURSION_LEVEL]) { framework ->
+        libsDir.traverse([type: FileType.FILES, maxDepth: ProjectHelper.MAX_RECURSION_LEVEL]) { framework ->
             if (framework == ".*[aA]pphance.*\\.framework") {
                 logger.lifecycle("Removing old apphance framework: " + framework.name)
                 def delClos = {
-                    it.eachDir( delClos );
+                    it.eachDir(delClos);
                     it.eachFile {
                         it.delete()
                     }
                 }
 
                 // Apply closure
-                delClos( new File(framework.canonicalPath) )
+                delClos(new File(framework.canonicalPath))
             }
         }
 
 
-        def projectApphanceZip =new File(libsDir, "apphance.zip")
+        def projectApphanceZip = new File(libsDir, "apphance.zip")
         projectApphanceZip.delete()
         def apphanceUrl = new URL("http://dev.polidea.pl/ext/32092342903/latest_ios_apphance.zip")
         projectApphanceZip << apphanceUrl.getContent()
@@ -129,7 +124,7 @@ class IOSApphancePlugin implements Plugin<Project> {
     boolean isApphancePresent(File projectDir) {
         def apphancePresent = false
 
-        projectDir.traverse([type: FileType.DIRECTORIES, maxDepth : ProjectHelper.MAX_RECURSION_LEVEL]) { framework ->
+        projectDir.traverse([type: FileType.DIRECTORIES, maxDepth: ProjectHelper.MAX_RECURSION_LEVEL]) { framework ->
             if (framework =~ ".*[aA]pphance.*\\.framework") {
                 apphancePresent = true
             }
@@ -144,7 +139,7 @@ class IOSApphancePlugin implements Plugin<Project> {
     }
 
     static public final String DESCRIPTION =
-"""This plugins provides automated adding of Apphance libraries to the project.
+        """This plugins provides automated adding of Apphance libraries to the project.
 """
 
 }
