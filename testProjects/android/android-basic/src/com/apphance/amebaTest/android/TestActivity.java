d66704edc71162de7791aaab61a18daf5c88d949
Maciej Opała <maciek.opala@gmail.com>
2013-01-30 12:23:12 +0100

    forgotten project

---
+++ testProjects/android/android-basic/src/com/apphance/amebaTest/android/TestActivity.java
@@ -0,0 +1,18 @@
+package com.apphance.amebaTest.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Test activity
+ */
+public class TestActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
