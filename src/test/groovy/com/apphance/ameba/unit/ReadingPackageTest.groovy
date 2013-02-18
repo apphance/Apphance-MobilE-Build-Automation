e11a1d5aa417952988810aa6846193962dc4123b
Maciej Opała <maciek.opala@gmail.com>
2013-02-18 12:50:08 +0100

    generating docs tests removed - new version will be implemented

--- src/test/groovy/com/apphance/ameba/unit/ReadingPackageTest.groovy
+++ src/test/groovy/com/apphance/ameba/unit/ReadingPackageTest.groovy
@@ -8,9 +8,9 @@ import static org.junit.Assert.assertArrayEquals
 class ReadingPackageTest {
     @Test
     public void testReadPackages() {
-        File f = new File("src/test/groovy")
+        File f = new File('src/test/groovy')
         def currentPackage = []
-        FileManager.findAllPackages("", f, currentPackage)
+        FileManager.findAllPackages('', f, currentPackage)
         currentPackage.sort()
         String[] packagesList = (String[]) [
                 'com.apphance.ameba',
@@ -18,13 +18,14 @@ class ReadingPackageTest {
                 'com.apphance.ameba.applyPlugins.android',
                 'com.apphance.ameba.applyPlugins.ios',
                 'com.apphance.ameba.conventions',
-                'com.apphance.ameba.documentation',
+                'com.apphance.ameba.detection',
+                'com.apphance.ameba.plugins',
                 'com.apphance.ameba.runBuilds.android',
                 'com.apphance.ameba.runBuilds.ios',
                 'com.apphance.ameba.unit',
-                'com.apphance.ameba.unit.ios',
                 'com.apphance.ameba.unit.android',
                 'com.apphance.ameba.unit.apphance.android',
+                'com.apphance.ameba.unit.ios',
                 'com.apphance.ameba.util'
         ].sort()
         assertArrayEquals(currentPackage.toString(), packagesList, (String[]) currentPackage)
