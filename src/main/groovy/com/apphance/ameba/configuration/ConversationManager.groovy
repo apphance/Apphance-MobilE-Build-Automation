19a3314dfedb6327d9fdfb54b7720d126ae3c854
Maciej Opała <maciek.opala@gmail.com>
2013-03-28 14:50:54 +0100

    conversation manager

--- src/main/groovy/com/apphance/ameba/configuration/ConversationManager.groovy
+++ src/main/groovy/com/apphance/ameba/configuration/ConversationManager.groovy
@@ -9,33 +9,67 @@ class ConversationManager {
     def reader = buildReader()
 
     def resolveConfigurations(List<Configuration> configurations) {
-
         configurations.each { Configuration c ->
-            if (!c.enabled) {
-                print "Enable plugin ${c.configurationName}? [y/n] "
-                out.flush()
-                String line = reader.readLine()
-                if (line.equalsIgnoreCase('y')) {
-                    c.enabled = true
-                }
-            }
-            if (c.enabled) {
-                c.amebaProperties.each { AbstractProperty ap ->
-                    print "${ap.message} [${ap.defaultValue()}]: "
-                    out.flush()
-                    readPropertyValue(ap)
-                }
-            }
+            enablePlugin(c)
+            readValues(c)
         }
-        //TODO serializer
     }
 
-    private void readPropertyValue(Prop ap) {
-        String line = reader.readLine()
-        if (line.trim().empty) {
+    @groovy.transform.PackageScope
+    void enablePlugin(Configuration c) {
+        if (!c.enabled) {
+            print "Enable plugin ${c.configurationName}? [y/n] "
+            out.flush()
+            if (reader.readLine()?.equalsIgnoreCase('y')) {
+                c.enabled = true
+            }
+        }
+    }
+
+    @groovy.transform.PackageScope
+    void readValues(Configuration c) {
+        if (c.enabled) {
+            c.amebaProperties.each { AbstractProperty ap ->
+                String input
+                while (true) {
+                    print prompt(ap)
+                    out.flush()
+                    input = reader.readLine()
+                    if (validateInput(ap, input))
+                        break
+                }
+                setPropertyValue(ap, input)
+            }
+        }
+    }
+
+    @groovy.transform.PackageScope
+    String prompt(AbstractProperty ap) {
+        "${ap.message}${defaultValueString(ap)}${possibleValuesString(ap)}: "
+    }
+
+    @groovy.transform.PackageScope
+    String defaultValueString(AbstractProperty ap) {
+        (ap.defaultValue && ap.defaultValue()) ? ", default: '${ap.defaultValue()}'" : ''
+    }
+
+    @groovy.transform.PackageScope
+    String possibleValuesString(AbstractProperty ap) {
+        ap.possibleValues ? ", possible: ${ap.possibleValues}" : ''
+    }
+
+    @groovy.transform.PackageScope
+    boolean validateInput(AbstractProperty ap, String input) {
+        input = input?.trim()
+        input?.empty || (input in ap.possibleValues) || (ap.validator && ap.validator(input))
+    }
+
+    @groovy.transform.PackageScope
+    void setPropertyValue(AbstractProperty ap, String input) {
+        if (input?.empty) {
             ap.value = ap.defaultValue()
-        } else {
-            ap.value = line
+        } else if (input in ap.possibleValues || (ap.validator && ap.validator(input))) {
+            ap.value = input
         }
     }
 
---
+++ src/test/groovy/unit/com/apphance/ameba/configuration/ConversationManagerSpec.groovy
@@ -0,0 +1,84 @@
+package com.apphance.ameba.configuration
+
+import com.apphance.ameba.configuration.properties.FileProperty
+import com.apphance.ameba.configuration.properties.LongProperty
+import com.apphance.ameba.configuration.properties.ProjectTypeProperty
+import com.apphance.ameba.configuration.properties.StringProperty
+import com.apphance.ameba.detection.ProjectType
+import spock.lang.Specification
+
+import static com.apphance.ameba.detection.ProjectType.ANDROID
+import static java.lang.System.getProperties
+
+class ConversationManagerSpec extends Specification {
+
+    def cm = new ConversationManager()
+
+    def 'possible value string is formatted correctly'() {
+        expect:
+        cm.possibleValuesString(p) == expectedString
+
+        where:
+        p                                                                            | expectedString
+        new StringProperty()                                                         | ''
+        new StringProperty(possibleValues: ['a', 'b'])                               | ', possible: [a, b]'
+        new ProjectTypeProperty(possibleValues: ProjectType.values()*.name().sort()) | ', possible: [ANDROID, IOS]'
+    }
+
+    def 'default value string is formatted correctly'() {
+        expect:
+        cm.defaultValueString(p) == expectedString
+
+        where:
+        p                                                  | expectedString
+        new StringProperty()                               | ''
+        new StringProperty(defaultValue: { null })         | ''
+        new StringProperty(defaultValue: { '' })           | ''
+        new StringProperty(defaultValue: { 'a' })          | ", default: 'a'"
+        new ProjectTypeProperty(defaultValue: { ANDROID }) | ", default: 'ANDROID'"
+    }
+
+    def 'prompt is displayed well'() {
+        expect:
+        cm.prompt(p) == expectedString
+
+        where:
+        p                                                                                              | expectedString
+        new StringProperty(message: 'Project name')                                                    | 'Project name: '
+        new StringProperty(message: 'Project name', defaultValue: { 'a' })                             | "Project name, default: 'a': "
+        new StringProperty(message: 'Project name', defaultValue: { 'a' }, possibleValues: ['a', 'b']) | "Project name, default: 'a', possible: [a, b]: "
+        new StringProperty(message: 'Project name', possibleValues: ['a', 'b'])                        | "Project name, possible: [a, b]: "
+    }
+
+    def 'input validation works well'() {
+        expect:
+        cm.validateInput(p, input) == validationResult
+
+        where:
+        p                                                                                   | input  | validationResult
+        new StringProperty()                                                                | ''     | true
+        new StringProperty()                                                                | '\n '  | true
+        new StringProperty()                                                                | 'v1'   | false
+        new StringProperty(possibleValues: ['v1', 'v2'])                                    | 'v1'   | true
+        new StringProperty(possibleValues: ['v1', 'v2'])                                    | 'v3'   | false
+        new StringProperty(possibleValues: ['a', 'b'], validator: { it.matches('[0-9]+') }) | '1234' | true
+        new StringProperty(possibleValues: ['a', 'b'], validator: { it.matches('[0-9]+') }) | 'a'    | true
+        new StringProperty(possibleValues: ['a', 'b'], validator: { it.matches('[0-9]+') }) | 'c'    | false
+    }
+
+    def 'property value is set well'() {
+        when:
+        cm.setPropertyValue(p, input)
+
+        then:
+        p.hashCode()
+        p.value == expectedValue
+
+        where:
+        p                                                      | input                                   | expectedValue
+        new StringProperty(defaultValue: { 'lol' })            | ''                                      | 'lol'
+        new StringProperty()                                   | ''                                      | null
+        new LongProperty(validator: { it.matches('[0-9]') })   | '5'                                     | 5L
+        new FileProperty(validator: { new File(it).exists() }) | properties['java.io.tmpdir'].toString() | new File(properties['java.io.tmpdir'].toString())
+    }
+}
