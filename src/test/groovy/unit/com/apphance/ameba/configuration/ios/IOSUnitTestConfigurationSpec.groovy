60c70a421b7d570c60e3173db68460385f2623dd
Arkadiusz Konior <arkadiusz.konior@polidea.pl>
2013-05-27 12:30:16 +0200

    unit test fix

--- src/test/groovy/unit/com/apphance/ameba/configuration/ios/IOSUnitTestConfigurationSpec.groovy
+++ src/test/groovy/unit/com/apphance/ameba/configuration/ios/IOSUnitTestConfigurationSpec.groovy
@@ -2,6 +2,7 @@ package com.apphance.ameba.configuration.ios
 
 import com.apphance.ameba.configuration.ios.variants.AbstractIOSVariant
 import com.apphance.ameba.configuration.ios.variants.IOSVariantsConfiguration
+import com.apphance.ameba.configuration.properties.ListStringProperty
 import com.apphance.ameba.configuration.properties.StringProperty
 import spock.lang.Specification
 
@@ -13,6 +14,7 @@ class IOSUnitTestConfigurationSpec extends Specification {
         def var1 = GroovyStub(AbstractIOSVariant) { getName() >> 'variantName1' }
         def var2 = GroovyStub(AbstractIOSVariant) { getName() >> 'variantName2' }
         iOSVariantsConf.@variants = [var1, var2]
+        iOSVariantsConf.variantsNames = new ListStringProperty(value: ['variantName1', 'variantName2'])
 
         def testConf = new IOSUnitTestConfiguration()
         testConf.iosVariantsConf = iOSVariantsConf
