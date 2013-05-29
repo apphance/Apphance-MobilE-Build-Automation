5fabf26c5a9a9bba9c76deeaf3c6abbbe300e935
Maciej Opala <maciek.opala@gmail.com>
2013-05-29 16:22:07 +0200

    unlock keychain spec + throws exception when no pass nor location

--- src/main/groovy/com/apphance/ameba/plugins/ios/buildplugin/tasks/UnlockKeyChainTask.groovy
+++ src/main/groovy/com/apphance/ameba/plugins/ios/buildplugin/tasks/UnlockKeyChainTask.groovy
@@ -4,12 +4,13 @@ import com.apphance.ameba.configuration.reader.PropertyReader
 import com.apphance.ameba.executor.command.Command
 import com.apphance.ameba.executor.command.CommandExecutor
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
 
 import javax.inject.Inject
 
 import static com.apphance.ameba.plugins.AmebaCommonBuildTaskGroups.AMEBA_BUILD
-import static org.gradle.api.logging.Logging.getLogger
+import static org.apache.commons.lang.StringUtils.isNotEmpty
 
 class UnlockKeyChainTask extends DefaultTask {
 
@@ -19,8 +20,6 @@ class UnlockKeyChainTask extends DefaultTask {
                             |or OSX_KEYCHAIN_PASSWORD and OSX_KEYCHAIN_LOCATION environment variable""".stripMargin()
     String group = AMEBA_BUILD
 
-    private l = getLogger(getClass())
-
     @Inject CommandExecutor executor
     @Inject PropertyReader reader
 
@@ -28,7 +27,7 @@ class UnlockKeyChainTask extends DefaultTask {
     void unlockKeyChain() {
         def pass = reader.systemProperty('osx.keychain.password') ?: reader.envVariable('OSX_KEYCHAIN_PASSWORD') ?: null
         def location = reader.systemProperty('osx.keychain.location') ?: reader.envVariable('OSX_KEYCHAIN_PASSWORD') ?: null
-        if (location != null && pass != null) {
+        if (isNotEmpty(pass) && isNotEmpty(location)) {
             executor.executeCommand(new Command(runDir: project.rootDir, cmd: [
                     'security',
                     'unlock-keychain',
@@ -37,7 +36,10 @@ class UnlockKeyChainTask extends DefaultTask {
                     location]
             ))
         } else {
-            l.warn("Seems that no keychain parameters are provided. Skipping unlocking the keychain.")
+            throw new GradleException("""|No keychain parameters provided. To unlock the keychain,
+                                         |pass osx.keychain.password and osx.keychain.location
+                                         |as java system properties (-D) or set OSX_KEYCHAIN_PASSWORD and
+                                         |OSX_KEYCHAIN_LOCATION environment variables""".stripMargin())
         }
     }
 }
---
+++ src/test/groovy/unit/com/apphance/ameba/plugins/ios/buildplugin/tasks/UnlockKeyChainTaskSpec.groovy
@@ -0,0 +1,51 @@
+package com.apphance.ameba.plugins.ios.buildplugin.tasks
+
+import com.apphance.ameba.TestUtils
+import com.apphance.ameba.configuration.reader.PropertyReader
+import com.apphance.ameba.executor.command.CommandExecutor
+import org.gradle.api.GradleException
+import spock.lang.Specification
+
+@Mixin(TestUtils)
+class UnlockKeyChainTaskSpec extends Specification {
+
+    def task = create(UnlockKeyChainTask)
+
+    def 'exception is thrown when no keychain properties set'() {
+        given:
+        task.reader = new PropertyReader()
+
+        when:
+        task.unlockKeyChain()
+
+        then:
+        def e = thrown(GradleException)
+        e.message == """|No keychain parameters provided. To unlock the keychain,
+                                         |pass osx.keychain.password and osx.keychain.location
+                                         |as java system properties (-D) or set OSX_KEYCHAIN_PASSWORD and
+                                         |OSX_KEYCHAIN_LOCATION environment variables""".stripMargin()
+    }
+
+    def 'keychain is unlocked'() {
+        given:
+        System.setProperty('osx.keychain.location', 'loc')
+        System.setProperty('osx.keychain.password', 'pass')
+
+        and:
+        def executor = GroovyMock(CommandExecutor)
+
+        and:
+        task.reader = new PropertyReader()
+        task.executor = executor
+
+        when:
+        task.unlockKeyChain()
+
+        then:
+        1 * executor.executeCommand({ it.commandForExecution == ['security', 'unlock-keychain', '-p', 'pass', 'loc'] })
+
+        cleanup:
+        System.properties.remove('osx.keychain.location')
+        System.properties.remove('osx.keychain.password')
+    }
+}
