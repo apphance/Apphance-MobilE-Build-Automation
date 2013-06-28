f5e5712ce4dcee401ba1a40460418b39dfa2d88b
Arkadiusz Konior <arkadiusz.konior@polidea.pl>
2013-06-28 13:58:08 +0200

    refactoring main method

--- src/main/groovy/com/apphance/flow/plugins/android/apphance/Main.groovy
+++ src/main/groovy/com/apphance/flow/plugins/android/apphance/Main.groovy
@@ -8,22 +8,27 @@ import static com.apphance.flow.configuration.apphance.ApphanceMode.QA
 class Main {
 
     public static void main(String[] args) {
-        println "Apphance Flow main method"
-        if(args.size() == 2) {
-            String mode = args[0]
-            assert mode in ['prod', 'pre']
-            def apphanceKey = args[1]
-            File currentDir = new File('.')
-            File manifest = new File(currentDir, 'AndroidManifest.xml')
-            assert manifest.exists(), "It is not an Android project. Run this application inside root directory of your app"
+        println "Standalone apphance adding tool"
+        if (args.size() != 2) usage("Bad number of argument. Got: ${args.size()}, should be: 2")
+        String mode = args[0]
+        if (!(mode in ['prod', 'pre'])) usage('Incorrect mode. Should be one of: pre, prod')
 
-            println "Adding apphance in mode: $mode, key: $apphanceKey"
-
-            def apphance = new AddApphanceToAndroid(currentDir, apphanceKey, mode == 'prod' ? PROD : QA, '1.9-RC1')
-            apphance.addApphance()
-
-        } else {
-            println "Usage: java -jar flow.jar [prod|pre] <APPHANCE_KEY> "
+        def apphanceKey = args[1]
+        File currentDir = new File('.')
+        File manifest = new File(currentDir, 'AndroidManifest.xml')
+        if (!manifest.exists()) {
+            usage("It is not an Android project. Run this application inside root directory of your app")
         }
+
+        println "Adding apphance in mode: $mode, key: $apphanceKey"
+
+        def apphance = new AddApphanceToAndroid(currentDir, apphanceKey, mode == 'prod' ? PROD : QA, '1.9-RC1')
+        apphance.addApphance()
+    }
+
+    def static usage(String message = null) {
+        if (message) println "ERROR: $message"
+        println "Usage: java -jar path-to-flow.jar [prod|pre] <APPHANCE_KEY> "
+        System.exit(1)
     }
 }
