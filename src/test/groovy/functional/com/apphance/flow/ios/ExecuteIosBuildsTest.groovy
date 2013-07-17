32d39be7ec1be31bf1c045912bcfd502bf0324ab
Maciej Opala <maciek.opala@gmail.com>
2013-07-17 13:34:52 +0200

    iOS - test class name fixed

--- src/test/groovy/functional/com/apphance/flow/ios/ExecuteIosBuildsTest.groovy
+++ src/test/groovy/functional/com/apphance/flow/ios/ExecuteIosBuildsTest.groovy
@@ -8,7 +8,7 @@ import org.junit.Test
 import static org.gradle.tooling.GradleConnector.newConnector
 import static org.junit.Assert.assertTrue
 
-class q {
+class ExecuteIosBuildsTest {
 
     public static final List<String> GRADLE_DAEMON_ARGS = ['-XX:MaxPermSize=1024m', '-XX:+CMSClassUnloadingEnabled',
             '-XX:+CMSPermGenSweepingEnabled', '-XX:+HeapDumpOnOutOfMemoryError', '-Xmx1024m'] as String[]
