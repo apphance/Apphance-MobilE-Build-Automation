d8c33a728139fb468fc8c26678700d18452b7776
Arkadiusz Konior <arkadiusz.konior@polidea.com>
2013-08-22 09:41:47 +0200

    android analysis. checkstyle, pmd, findbugs

--- src/main/groovy/com/apphance/flow/plugins/android/analysis/AndroidAnalysisPlugin.groovy
+++ src/main/groovy/com/apphance/flow/plugins/android/analysis/AndroidAnalysisPlugin.groovy
@@ -1,60 +1,53 @@
 package com.apphance.flow.plugins.android.analysis
 
 import com.apphance.flow.configuration.android.AndroidAnalysisConfiguration
-import com.apphance.flow.plugins.android.analysis.tasks.CPDTask
-import com.apphance.flow.plugins.android.analysis.tasks.CheckstyleTask
-import com.apphance.flow.plugins.android.analysis.tasks.FindBugsTask
-import com.apphance.flow.plugins.android.analysis.tasks.PMDTask
-import com.apphance.flow.plugins.android.buildplugin.tasks.CompileAndroidTask
+import com.apphance.flow.configuration.android.variants.AndroidVariantConfiguration
+import com.apphance.flow.configuration.android.variants.AndroidVariantsConfiguration
+import com.apphance.flow.util.file.FileManager
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 import javax.inject.Inject
 
-import static com.apphance.flow.plugins.FlowTasksGroups.FLOW_ANALYSIS
 import static org.gradle.api.logging.Logging.getLogger
 
 /**
  * Provides static code analysis.
- *
  */
 class AndroidAnalysisPlugin implements Plugin<Project> {
 
-    def log = getLogger(this.class)
+    def logger = getLogger(this.class)
 
     @Inject AndroidAnalysisConfiguration analysisConf
+    @Inject AndroidVariantsConfiguration androidVariantsConf
 
     @Override
     void apply(Project project) {
         if (analysisConf.isEnabled()) {
-            log.lifecycle("Applying plugin ${this.class.simpleName}")
+            logger.lifecycle "Applying plugin ${this.class.simpleName}"
 
-            project.configurations.create('pmdConf')
-            project.dependencies.add('pmdConf', 'pmd:pmd:4.3')
-            project.task(PMDTask.NAME,
-                    type: PMDTask)
+            project.apply plugin: 'java'
+            project.apply plugin: 'pmd'
+            project.apply plugin: 'checkstyle'
+            project.apply plugin: 'findbugs'
 
-            project.task(CPDTask.NAME,
-                    type: CPDTask)
+            project.compileJava.enabled = false
+            project.compileTestJava.enabled = false
+            project.processResources.enabled = false
+            project.processTestResources.enabled = false
 
-            project.configurations.create('findbugsConf')
-            project.dependencies.add('findbugsConf', 'com.google.code.findbugs:findbugs:2.0.1')
-            project.dependencies.add('findbugsConf', 'com.google.code.findbugs:findbugs-ant:2.0.1')
-            project.task(FindBugsTask.NAME,
-                    type: FindBugsTask,
-                    dependsOn: CompileAndroidTask.NAME)
+            project.checkstyleMain.ignoreFailures = true
+            project.findbugsMain.ignoreFailures = true
+            project.pmd.ignoreFailures = true
 
+            def mainVariant = androidVariantsConf.variants.find { it.name == androidVariantsConf.mainVariant }
 
-            project.configurations.create('checkstyleConf')
-            project.dependencies.add('checkstyleConf', 'com.puppycrawl.tools:checkstyle:5.6')
-            project.task(CheckstyleTask.NAME,
-                    type: CheckstyleTask,
-                    dependsOn: CompileAndroidTask.NAME)
+            project.findbugsMain.dependsOn mainVariant.buildTaskName
+            project.findbugsTest.dependsOn mainVariant.buildTaskName
 
-            project.task('analysis',
-                    description: 'Runs all analysis on project',
-                    group: FLOW_ANALYSIS,
-                    dependsOn: [FindBugsTask.NAME, PMDTask.NAME, CPDTask.NAME, CheckstyleTask.NAME])
+            project.sourceSets.main.java { srcDir 'src' }
+            def variantDir = FileManager.relativeTo(project.rootDir, mainVariant.tmpDir)
+            project.sourceSets.main.output.classesDir = variantDir + '/bin/classes'
         }
     }
 }
