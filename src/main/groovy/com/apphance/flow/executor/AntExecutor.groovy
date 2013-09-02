a46319b5e5071554a61878086ba5e25bccfbe029
Arkadiusz Konior <arkadiusz.konior@polidea.com>
2013-09-02 15:48:35 +0200

    removing unnecessary catch

--- src/main/groovy/com/apphance/flow/executor/AntExecutor.groovy
+++ src/main/groovy/com/apphance/flow/executor/AntExecutor.groovy
@@ -3,7 +3,6 @@ package com.apphance.flow.executor
 import com.apphance.flow.executor.command.Command
 import com.apphance.flow.executor.command.CommandExecutor
 import com.apphance.flow.util.FlowUtils
-import org.gradle.api.GradleException
 
 import javax.inject.Inject
 import javax.inject.Named
@@ -23,15 +22,11 @@ class AntExecutor {
     @Named('executable.ant') ExecutableCommand executableAnt
 
     void executeTarget(File rootDir, String command) {
-        try {
-            def signProps = System.getProperties().findAll { it.key.startsWith('key.') }
-            def commandProps = signProps.keySet().collect { "-D$it=\$${dotToCamel(it)}" }
-            def secretParams = signProps.collectEntries { String key, String value -> [(dotToCamel(key)): value] }
+        def signProps = System.getProperties().findAll { it.key.startsWith('key.') }
+        def commandProps = signProps.keySet().collect { "-D$it=\$${dotToCamel(it)}" }
+        def secretParams = signProps.collectEntries { String key, String value -> [(dotToCamel(key)): value] }
 
-            executor.executeCommand new Command(runDir: rootDir, cmd: executableAnt.cmd + [command] + commandProps, failOnError: true, secretParams: secretParams)
-        } catch (IOException e) {
-            throw new GradleException("Error during execution: ant $command", e)
-        }
+        executor.executeCommand new Command(runDir: rootDir, cmd: executableAnt.cmd + [command] + commandProps, failOnError: true, secretParams: secretParams)
     }
 }
 
