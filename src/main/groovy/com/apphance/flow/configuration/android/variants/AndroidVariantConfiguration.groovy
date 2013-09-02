6e45874dcea42d3fd772d3ed279539d52f0caeb7
Arkadiusz Konior <arkadiusz.konior@polidea.com>
2013-09-02 14:30:53 +0200

    validate signing configuration

--- src/main/groovy/com/apphance/flow/configuration/android/variants/AndroidVariantConfiguration.groovy
+++ src/main/groovy/com/apphance/flow/configuration/android/variants/AndroidVariantConfiguration.groovy
@@ -88,17 +88,31 @@ class AndroidVariantConfiguration extends AbstractVariant {
     }
 
     void checkSigningConfiguration() {
-        def file = new File(tmpDir, 'ant.properties')
-        check file.exists(), "If release or apphance plugin is enabled ant.properties should be present in ${tmpDir.absolutePath}"
+        def signParams = ['key.store', 'key.store.password', 'key.alias', 'key.alias.password']
+        def nonemptySigningProperties = signParams.collect { System.getProperty(it) }.grep()
 
+        def file = new File(tmpDir, 'ant.properties')
+        check file.exists() || nonemptySigningProperties.size() == 4, "If release or apphance plugin is enabled ant.properties should be present in " +
+                "${tmpDir.absolutePath} or appropriate signing parameters ($signParams) passed to gradle as command line options"
+
+        Properties antProperties = null
         if (file.exists()) {
-            Properties antProperties = asProperties(file)
-            String keyStorePath = antProperties.getProperty('key.store')
-            check keyStorePath, "key.store value in ant.properties file is not correctly configured: ${keyStorePath}"
-            if (keyStorePath) {
-                def keyStore = Paths.get(tmpDir.absolutePath).resolve(keyStorePath).toFile()
-                check keyStore?.exists(), "Keystore path is not correctly configured: File ${keyStore.absolutePath} doesn't exist."
-            }
+            antProperties = asProperties(file)
         }
+        String keyStorePath = validate(antProperties, 'key.store')
+        if (keyStorePath) {
+            def keyStore = Paths.get(tmpDir.absolutePath).resolve(keyStorePath).toFile()
+            check keyStore?.exists(), "Keystore path is not correctly configured: File ${keyStore?.absolutePath} doesn't exist."
+        }
+
+        validate(antProperties, 'key.store.password')
+        validate(antProperties, 'key.alias')
+        validate(antProperties, 'key.alias.password')
+    }
+
+    String validate(Properties antProperties, String property) {
+        String value = antProperties?.getProperty(property) ?: System.getProperty(property)
+        check value, "$property value is not correctly configured: ${value}"
+        value
     }
 }
