c42c18cb557a8bd54960e61f01fa354a5321a654
Arkadiusz Konior <arkadiusz.konior@polidea.com>
2013-09-02 14:25:32 +0200

    pass sign properties to command executor

--- src/main/groovy/com/apphance/flow/executor/AntExecutor.groovy
+++ src/main/groovy/com/apphance/flow/executor/AntExecutor.groovy
@@ -2,6 +2,7 @@ package com.apphance.flow.executor
 
 import com.apphance.flow.executor.command.Command
 import com.apphance.flow.executor.command.CommandExecutor
+import com.apphance.flow.util.FlowUtils
 import org.gradle.api.GradleException
 
 import javax.inject.Inject
@@ -10,6 +11,7 @@ import javax.inject.Named
 /**
  * Executor of ant targets.
  */
+@Mixin(FlowUtils)
 class AntExecutor {
 
     public static String DEBUG = 'debug'
@@ -20,11 +22,16 @@ class AntExecutor {
     @Inject
     @Named('executable.ant') ExecutableCommand executableAnt
 
-    void executeTarget(File rootDir, String command, Map params = [:]) {
+    void executeTarget(File rootDir, String command) {
         try {
-            executor.executeCommand(new Command([runDir: rootDir, cmd: executableAnt.cmd + [command], failOnError: true] + params))
+            def signProps = System.getProperties().findAll { it.key.startsWith('key.') }
+            def commandProps = signProps.keySet().collect { "-D$it=\$${dotToCamel(it)}" }
+            def secretParams = signProps.collectEntries { String key, String value -> [(dotToCamel(key)): value] }
+
+            executor.executeCommand new Command(runDir: rootDir, cmd: executableAnt.cmd + [command] + commandProps, failOnError: true, secretParams: secretParams)
         } catch (IOException e) {
-            throw new GradleException("Error during execution: ant $command, $params", e)
+            throw new GradleException("Error during execution: ant $command", e)
         }
     }
 }
+
