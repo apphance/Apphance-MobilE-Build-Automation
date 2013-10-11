afad14475ebac80faaa0b3f06376558095a26c6c
Maciej Opala <maciek.opala@gmail.com>
2013-10-11 11:03:42 +0200

    iOS - plist parser refactor

--- src/main/groovy/com/apphance/flow/plugins/ios/parsers/PlistParser.groovy
+++ src/main/groovy/com/apphance/flow/plugins/ios/parsers/PlistParser.groovy
@@ -10,7 +10,6 @@ import javax.inject.Inject
 import java.util.regex.Pattern
 
 import static org.apache.commons.lang.StringUtils.isBlank
-import static org.apache.commons.lang.StringUtils.isNotBlank
 
 class PlistParser {
 
@@ -25,34 +24,30 @@ class PlistParser {
     @Inject IOSExecutor executor
 
     String bundleVersion(File plist) {
-        def json = parsedJson(plist)
-        json.CFBundleVersion
+        parsedJson.call(plist).CFBundleVersion
     }
 
     String bundleShortVersionString(File plist) {
-        def json = parsedJson(plist)
-        json.CFBundleShortVersionString
+        parsedJson.call(plist).CFBundleShortVersionString
     }
 
     String bundleId(File plist) {
-        def json = parsedJson(plist)
-        json.CFBundleIdentifier
+        parsedJson.call(plist).CFBundleIdentifier
     }
 
     String bundleDisplayName(File plist) {
-        def json = parsedJson(plist)
-        json.CFBundleDisplayName
+        parsedJson.call(plist).CFBundleDisplayName
     }
 
     List<String> iconFiles(File plist) {
-        def json = parsedJson(plist)
+        def json = parsedJson.call(plist)
         (json.CFBundleIconFiles + json.CFBundleIcons.CFBundlePrimaryIcon.CFBundleIconFiles)?.unique()?.sort()
     }
 
-    private Object parsedJson(File plist) {
+    private Closure<Map> parsedJson = { File plist ->
         def text = executor.plistToJSON(plist).join('\n')
-        new JsonSlurper().parseText(text)
-    }
+        new JsonSlurper().parseText(text) as Map
+    }.memoize()
 
     void replaceVersion(File plist, String versionCode, String versionString) {
         def xml = new XmlSlurper().parse(plist)
